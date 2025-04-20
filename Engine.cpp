d6ab6e81ec0c9274b1491ca18beda44165bff84e
Yuri Melo <yurifullstack@gmail.com>
2025-04-20 17:31:12 -0300

    Add nlohmann/json dependency and update Engine.cpp to use JSON serialization

--- Engine.cpp
+++ Engine.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
-#include <thread>
 #include <fmt/core.h>
+#include <nlohmann/json.hpp>
 
 #include <concurrency/concurrency.h>
 
+using json = nlohmann::json;
+
 int main()
 {
-	// Concurrency::Thams thams;
-	
-	// std::thread t1(&Concurrency::Thams::Increment, &thams);
-	// std::thread t2(&Concurrency::Thams::Increment, &thams);
-
-	// t1.join();
-	// t2.join();
-
-	// thams.Foo();
-
 	fmt::print("Hello, {}\n", "Thamirys");
+
+	// Cria um objeto JSON
+    json dados = {
+        {"nome", "João"},
+        {"idade", 30},
+        {"hobbies", {"ler", "correr", "programar"}}
+    };
+
+    // Serializa para string
+    std::string json_str = dados.dump(4);  // Indentação de 4 espaços
+
+    // Imprime
+    std::cout << "JSON Gerado:\n" << json_str << std::endl;
 	
 	return 0;
 }
